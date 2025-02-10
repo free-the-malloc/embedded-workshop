e585518f3305efa56261526868f7165baeffd24d
free-the-malloc <cooper.gee@hotmail.com>
2025-02-10 11:44:04 +1100

    some edits to ir_receiver.c

--- src/ir_receiver.c
+++ src/ir_receiver.c
@@ -1,73 +1,116 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include <avr/io.h>
 
 #include <serial.h>
 
+#define BIN_LEN (64)
+#define BUFF_LEN (256)
+
+#define INPUT_HIGH  (PINB & (1<<PINB0))
+#define INPUT_LOW   (!(PINB & (1<<PINB0)))
+
+#define BURST_PERIOD (562.2)
+#define CHECK_PERIOD (BURST_PERIOD * 2)
+
 
 /**
- * 0 is represented by a 562.2 microsecond burst followed by a 562.2
- * microsecond low period and a 1 is represented by a 562.2 microsecond 
- * burst followed by a 1,687 microsecond low period. 
+ * IR Receiver used: ZD1592
+ * IR Protocol: NEC
+ * Input Pin: B0
+ * 
+ * The ZD1592 works by pushing current through the specified input 
+ * pin, breaking that current when IR is received. As such, when IR 
+ * is received, the input on the pin should be read as low. 
+ * 
+ * The NEC IR communication protocol builds a message frame by the 
+ * following steps: 
+ *  1. A 9 millisecond burst of IR followed by a 4.5 millisecond low 
+ * period. 
+ * 
+ *  2. Now the actual message is constructed. A '0' is signalled by a
+ * 562.2 microsecond burst of IR followed by a 562.2 microsecond low 
+ * period. A '1' is signalled by a 562.2 microsecond burst of IR, 
+ * followed by 1,687 microsecond (or 1.687 millisecond) low period. 
+ * 
+ *  3. Typically, the 8-bit address of the device followed by it's 
+ * logical inverse is transmitted after the initial pulse burst and 
+ * low period. That is followed by the 8-bit message and it's logical
+ * inverse. 
+ * 
+ * Function writes the binary message read into the buffer supplied. 
+ * Returns 1 if unsuccessful and 0 if successful. 
  */
-void read_transmission( char *buffer )
+int NEC_read_transmission( char *buffer )
 {
-    unsigned int state = 0;
     unsigned int index = 0;
 
-    /* first wait out the initial transmission */
-    while (!(PINB & (1<<PINB0)));
+    _delay_ms(9);
 
-    /* wait for the 4.5 seconds of silence */
-    while (PINB & (1<<PINB0));
+    if (INPUT_LOW){ /* check if input is still high (it shouldn't be) */
+        return 1;
+    }
 
-    /* now read the message */
-    while (state <= 4){
-        if (state > 0){
-            _delay_us(565);
-            if (!(PINB & (1<<PINB0))){
-                buffer[index] = '0';
-                state = 0;
-                index++;
-                continue;
-            }
+    _delay_ms(4.5); /* 4.5ms low period */
 
-            _delay_us(1130);
-            if (!(PINB & (1<<PINB0))){
-                buffer[index] = '1';
-                state = 0;
-                index++;
-                continue;
-            } else {
-                break;
-            }
+    /**
+     * Begin reading the message. Each iteration of the loop 
+     * should begin with a pulse of IR, hence the initial if 
+     * statement. 
+     */
+    while (1){
+        if (INPUT_HIGH){
+            return 1;
         }
 
-        if (!(PINB & (1<<PINB0))){
-            while (!(PINB & (1<<PINB0)));
-
-            state = 1;
+        _delay_us(CHECK_PERIOD);
+        if (INPUT_LOW){
+            buffer[index] = '0';
+            index++;
             continue;
         }
+
+        _delay_us(CHECK_PERIOD);
+        if (INPUT_LOW){
+            buffer[index] = '1';
+            index++;
+            continue;
+        } 
+
         break;
     }
     buffer[index] = '\0';
+    return 0;
 }
+
 
 int main( void )
 {
-    int state;
-    char buffer[33];
+    char binary[BIN_LEN], *endptr, buffer[BUFF_LEN];
+    unsigned long hexval;
 
     USART0_Init();
     
     DDRB &= ~(1<<DDB0); //set PB0 as an input pin
+    PORTB |= (1<<PORTB0);
     
+    _delay_ms(500);
+
     while (1) {
-        if (!(PINB & (1<<PINB0))) { /* if pin B0 is low */
-            read_transmission(buffer);
+        if (INPUT_LOW) {
+            if (NEC_read_transmission(binary)){
+                _delay_ms(90);
+                continue;
+            }
+            hexval = strtol(binary, &endptr, 2);
+            snprintf(buffer, 256, "binary: %s\nhex: %06lx\n", binary, (unsigned long) hexval & 0xFFFFFFUL);
             USART0_PutString(buffer);
-            USART0_Transmit('\n');
         }
     }
+
+    return 1;
 }
